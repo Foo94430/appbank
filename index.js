4833c2fc50d3ff2b896e068b4e5457c0102fa554
Foo <foo@MacBook-Air-de-Foo.local>
2025-07-20 20:03:40 +0200

    mise a jour

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 const express = require("express");
 const port = process.env.PORT || 5550;
 const cors = require("cors");
-const { json } = require("express/lib/response");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-
-
-
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
+const stockage = [
+  {
+    data: "",
+  },
+];
 
 app.post("/", (req, res) => {
-const data = req.body
- 
-  res.status(201).json({ data : req.body});
-   const p = [].push(data)
-   console.log(p);
-});
+  const data = req.body;
 
+  res.status(201).json({ data: req.body });
+  localStorage.setItem("data", data);
+});
 
 app.get("/", (req, res) => {
   res.status(200).send(res.json());
 });
 
-
 app.listen(port, () => {
-  console.log("serveur démarrée");
+  console.log(`serveur démarrée sur le port ${port} `);
 });
--- nova.js
+++ nova.js
@@ -1,29 +1,44 @@
-const maj = /[A-Z]/;
+// =====================
+// Regex
 
+// =====================
+const maj = /^[A-Z]/;
+const caraSpecial = /["@", "#", "$", "%", "&", "!", "?",]/;
+
+// =====================
+// Elements du DOM
+// =====================
 const container = document.querySelector(".container");
 const signIn = document.querySelector(".signIn");
 const signUp = document.querySelector(".signUp");
+
+// =====================
+// Pages dynamiques
+// =====================
 const signInPage = document.createElement("div");
 signInPage.classList.add("signInPage", "container");
+
 const signUpPage = document.createElement("div");
 signUpPage.classList.add("signUpPage", "container");
+
+// =====================
+// Page Connexion (Sign In)
+// =====================
+signInPage.innerHTML = `
+  <h1 class="title"> Connexion </h1>
+  <form> 
+    <input placeholder="Identifiant" class="signInInput id" type="text">
+    <input placeholder="Mot de passe" class="signInInput" type="text"> 
+    <button> C'est parti ! </button>
+  <form>
+`;
 
 signIn.addEventListener("click", () => {
   container.replaceWith(signInPage);
 });
 
-signInPage.innerHTML = `
-<h1 class="title"> Connexion </h1>
-<form > 
-<input  placeholder="Identifiant" class="signInInput id" type="text">
-<input  placeholder="Mot de passe"  class="signInInput" type="text"> 
-<button> C'est parti ! </button>
-<form>
-
-`;
-
 const form = signInPage.querySelector("form");
-const inputId = signInPage.querySelector(" .id");
+const inputId = signInPage.querySelector(".id");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -37,26 +52,40 @@ form.addEventListener("submit", (event) => {
   }
 });
 
+// =====================
+// Page Inscription (Sign Up)
+// =====================
+signUpPage.innerHTML = `
+  <h1 class="title"> Créer votre compte </h1>
+  <form> 
+    <input placeholder="Nom" class="signInInput name" type="text">
+    <input placeholder="Prénom" class="signInInput firstname" type="text"> 
+    <input placeholder="Créer un mot de passe" class="signInInput password" type="text"> 
+    <input placeholder="Retaper votre mot de passe" class="signInInput retype" type="text"> 
+    <button> C'est parti ! </button>
+  <form>
+`;
+
 signUp.addEventListener("click", () => {
   container.replaceWith(signUpPage);
 });
 
-signUpPage.innerHTML = `
-<h1 class="title"> Créer votre compte </h1>
-<form > 
-<input  placeholder="Nom" class="signInInput" type="text">
-<input  placeholder="Prénom"  class="signInInput" type="text"> 
-<input  placeholder="Créer un mot de passe"  class="signInInput password" type="text"> 
-<input  placeholder="Retaper votre mot de passe"  class="signInInput" type="text"> 
-<button> C'est parti ! </button>
-<form>
-
-`;
-
 const password = signUpPage.querySelector(".password");
-const formSignUpPage = signUpPage.querySelector("form")
+const retype = signUpPage.querySelector(".retype");
+const nam = signUpPage.querySelector(".name");
+const firstName = signUpPage.querySelector(".firstname");
+const formSignUpPage = signUpPage.querySelector("form");
 
+// =====================
+// Affichage règles mot de passe
+// =====================
 const passwordRules = document.createElement("div");
+passwordRules.innerHTML = `
+  <p class="characteres"><span class=''></span> Le mot de passe doit contenir au moins 8 caractères. <p>
+  <p class="maj"><span class=" "></span>Doit contenir au moins une lettre majuscule (A–Z). <p>
+  <p class="special"><span class=""></span> Doit inclure au moins un caractère spécial (ex. @, #, $, %, &, !, ?, etc.).<p>
+  <p class="space"><span class=""></span> Ne doit pas contenir d'espace<p>
+`;
 
 password.addEventListener("click", () => {
   passwordRules.classList.add("passwordrules");
@@ -64,74 +93,87 @@ password.addEventListener("click", () => {
   signUpPage.style.height = "700px";
 });
 
-passwordRules.innerHTML = `
-   
-<p> <span class='' >
-</span>  Le mot de passe doit contenir au moins 8 caractères. 
- <p>
-<p> <span class=" ">
-</span>Doit contenir au moins une lettre majuscule (A–Z). 
-<p>
-<p><span class="">
-</span> Doit inclure au moins un caractère spécial (ex. @, #, $, %, &, !, ?, etc.).
-<p>
-<p><span class="">
-</span> Doit contenir une majuscules 
-<p>
-`;
-
-/* v.forEach((el) => {
-  el.addEventListener("click", () => {
-   
-    if (w) {
-  
-    }
-  });
-})*/
-
-
-
-
+// =====================
+// Vérification mot de passe en temps réel
+// =====================
 password.addEventListener("input", (event) => {
-  const v = passwordRules.querySelectorAll("p");
-  const w = passwordRules.querySelector("span");
-  password.classList.add("error");
   const value = password.value;
+  const characteresSpan = passwordRules.querySelector(".characteres > span");
+  const majSpan = passwordRules.querySelector(".maj > span");
+  const specialSpan = passwordRules.querySelector(".special > span");
+  const spaceSpan = passwordRules.querySelector(".space > span");
 
+  password.classList.add("error");
 
   if (value.length >= 8) {
-    w.classList.add("done");
-    password.classList.replace("error", "succes");
+    characteresSpan.classList.add("done");
   } else {
-    w.classList.remove("done");
-    password.classList.replace("succes", "error");
+    characteresSpan.classList.remove("done");
+  }
+
+  if (maj.test(value)) {
+    majSpan.classList.add("done");
+  } else {
+    majSpan.classList.remove("done");
+  }
+
+  if (caraSpecial.test(value)) {
+    specialSpan.classList.add("done");
+  } else {
+    specialSpan.classList.remove("done");
+  }
+
+  if (!/\s/.test(value)) {
+    spaceSpan.classList.add("done");
+  } else {
+    spaceSpan.classList.remove("done");
   }
 });
 
+// =====================
+// Soumission du formulaire inscription
+// =====================
+formSignUpPage.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-formSignUpPage.addEventListener('submit',event  => {
-event.preventDefault();
-  const value = password.value
-  const post = fetch ("http://localhost:5550/", {
-    method : "POST",
-    body : JSON.stringify({password : value}),
-    headers : {
-      "Content-Type" : "application/json"
+  const value = password.value;
+
+  const valueLength = value.length >= 8;
+  const valueMaj = maj.test(value);
+  const valueSpecial = caraSpecial.test(value);
+  const valueSpace = !/\s/.test(value);
+
+  const valid = valueLength && valueMaj && valueSpecial && valueSpace;
+  const formIsvalid =
+    retype.value && nam.value && firstName.value && password.value;
+
+  if (formIsvalid === "") {
+    console.error("Veuillez remplir les champs vides");
+  } else {
+    if (!valid) {
+      console.error("Le champs n'est pas rempli ! ");
+    } else {
+      if (retype.value !== value) {
+        console.error(
+          " Les mots de passe ne correspondent pas. Veuillez réessayer."
+        );
+      } else {
+        const post = fetch("http://localhost:5550/", {
+          method: "POST",
+          body: JSON.stringify({
+            nam: nam.value,
+            firstname: firstName.value,
+            password: value,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        post.then(async (response) => {
+          const res = await response.json();
+          console.log(res);
+        });
+      }
     }
-  })
-
-  post.then( async (response) => {
-    const res = await response.json()
-    console.log(res)
-  })
-
-  const get = fetch ("http://localhost:5550/")
-  get.then( async response => {
-    const getres = await response.json()
-    console.log(getres)
-  })
-
-})
-
-
-
+  }
+});
